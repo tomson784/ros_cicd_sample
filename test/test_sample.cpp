8776213e6798e2d901ecea00448a50d569f9f650
tomson784 <tomohito040759@gmail.com>
2021-10-27 09:55:21 +0900

    update

---
+++ test/test_sample.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+
+#include <ros/ros.h>
+
+TEST(SampleTest, TestCase1)
+{
+    EXPECT_EQ(true, true);
+}
+
+
+
+int main(int argc, char** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  ros::init(argc, argv, "test_sample");
+  ros::NodeHandle nh;
+  return RUN_ALL_TESTS();
+}
